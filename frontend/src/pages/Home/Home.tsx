feb18ed9051875362366632e5e54f74ec78fe5d0
XanetZaldua <xanet.zaldua@alumni.mondragon.edu>
2025-11-08 21:42:30 +0100

    general-fixes

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import './../../App.css'
+
+function Home() {
+  return (
+    <>
+        <h1>
+            WELLCOME TO BATTLENET!
+        </h1>
+        <p>Made by Mikel Etxarri, Aitzol Moreno, Ibon Muramendiaraz and Xanet Zaldua</p>
+    </>
+  )
+}
+
+export default Home
